99711358f8619092c5fc5d2f879fd59f05c4c7e6
JulianNM <julinavarromoya01@gmail.com>
2026-09-18 12:01:23 +0200

    version final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,73 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
+        ArrayList<String> tareas = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+        System.out.println();
+        System.out.println("|----------------------------------|");
+        System.out.println("|                                  |");
+        System.out.println("|  Bienvenido al gestor de tareas  |");
+        System.out.println("|                                  |");
+        System.out.println("|----------------------------------|");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        do {
+
+            System.out.println();
+            System.out.println("      ----- MENU -----");
+            System.out.println();
+            System.out.println(" - 1. Anadir una tarea  ");
+            System.out.println(" - 2. Ver lista de tareas  ");
+            System.out.println(" - 3. Marcar tarea como completada  ");
+            System.out.println(" - 4. Eliminar una tarea  ");
+            System.out.println(" - 5. Salir ");
+            System.out.println("");
+            System.out.print("Elige una opcion: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Descripcion de la tarea: ");
+                    String nuevaTarea = scanner.nextLine();
+                    tareas.add("[ ] " + nuevaTarea);
+                    System.out.println("Tarea anadida.");
+                    break;
+
+                case 2:
+                    System.out.println("--- TAREAS ---");
+                    for (int i = 0; i < tareas.size(); i++) {
+                        System.out.println((i + 1) + ". " + tareas.get(i));
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Numero de la tarea a completar: ");
+                    int numCompletar = scanner.nextInt();
+                    String tareaAntigua = tareas.get(numCompletar - 1);
+                    String tareaActualizada = "[X] " + tareaAntigua.substring(4);
+                    tareas.set(numCompletar - 1, tareaActualizada);
+                    System.out.println("Tarea completada.");
+                    break;
+
+                case 4:
+                    System.out.print("Numero de la tarea a eliminar: ");
+                    int numEliminar = scanner.nextInt();
+                    tareas.remove(numEliminar - 1);
+                    System.out.println("Tarea eliminada.");
+                    break;
+
+                case 5:
+                    System.out.println("Saliendo del programa...");
+                    break;
+
+                default:
+                System.out.println("Opcion incorrecta.");
+            }
+        } while (opcion != 5);
+        scanner.close();
     }
 }
